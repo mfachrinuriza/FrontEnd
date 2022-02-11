f1e3ec7945e311d00f1673bcdba15adf8a021393
Muhammad Fachri Nuriza <mfachrinuriza@gmail.com>
2022-02-12 00:51:52 +0700

    Triangles

---
+++ src/triangle/Triangle.js
@@ -0,0 +1,13 @@
+import './Triangle.css'
+import Triangles from '../triangles/Triangles'
+
+const Triangle = () => {
+    return(
+        <div>
+            <h2>Triangles</h2>
+            <Triangles />
+        </div>
+    )
+}
+
+export default Triangle
---
+++ src/triangles/Triangles.js
@@ -0,0 +1,14 @@
+import './Triangles.css'
+
+const Triangles = () => {
+    return (
+        <div>
+            <div class="arrow-up-cream"></div>
+            <div class="arrow-up-chocolate"></div>
+            <div class="arrow-up-green"></div>
+        </div>
+        
+    )
+}
+
+export default Triangles
